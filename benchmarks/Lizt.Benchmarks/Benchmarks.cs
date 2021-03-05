0a4d49db7cc189273ca6214a39399129b3e1bc60
TonuFish <Arkaicore@outlook.com>
2021-03-05 15:16:15 +0800

    Add project files.

---
+++ benchmarks/Lizt.Benchmarks/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Lizt.Benchmarks
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
